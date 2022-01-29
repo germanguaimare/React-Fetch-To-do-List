ce27d55d50ade246a21c6d0a1d79d84f9a07e25e
germanguaimare <germanguaimare@gmail.com>
2022-01-29 18:51:54 +0000

    final commit

---
+++ src/js/component/card.jsx
@@ -0,0 +1,134 @@
+import React, { useEffect, useState, useProps } from "react";
+import { Card, CardTitle, Button, CardBody } from "reactstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBackspace } from "@fortawesome/free-solid-svg-icons";
+
+const ListCard = (props) => {
+	useEffect(() => {
+		getList();
+	}, []);
+	const list = props.list;
+	const setList = props.setList;
+	const [newtask, setnewTask] = useState("");
+	const getList = () => {
+		var requestOptions = {
+			method: "GET",
+			redirect: "follow",
+		};
+
+		fetch(
+			"https://assets.breatheco.de/apis/fake/todos/user/germanguaimare",
+			requestOptions
+		)
+			.then((response) => response.json())
+			.then((result) => {
+				setList(result);
+			})
+			.catch((error) => console.log("error", error));
+	};
+	const addTask = () => {
+		if (newtask != "") {
+			list.push({ label: newtask, done: false });
+			var myHeaders = new Headers();
+			myHeaders.append("Content-Type", "application/json");
+
+			var raw = list.map((key, index) => ({
+				label: list[index].label,
+				done: false,
+			}));
+
+			var requestOptions = {
+				method: "PUT",
+				headers: myHeaders,
+				body: JSON.stringify(raw),
+				redirect: "follow",
+			};
+
+			fetch(
+				"https://assets.breatheco.de/apis/fake/todos/user/germanguaimare",
+				requestOptions
+			)
+				.then((response) => response.text())
+				.then((result) => {
+					getList();
+					setnewTask("");
+				})
+				.catch((error) => console.log("error", error));
+		} else {
+			var myHeaders = new Headers();
+			myHeaders.append("Content-Type", "application/json");
+
+			var raw = list.map((key, index) => ({
+				label: list[index].label,
+				done: false,
+			}));
+
+			var requestOptions = {
+				method: "PUT",
+				headers: myHeaders,
+				body: JSON.stringify(raw),
+				redirect: "follow",
+			};
+
+			fetch(
+				"https://assets.breatheco.de/apis/fake/todos/user/germanguaimare",
+				requestOptions
+			)
+				.then((response) => response.text())
+				.then((result) => {
+					getList();
+					setnewTask("");
+				})
+				.catch((error) => console.log("error", error));
+		}
+	};
+	const deleteTask = (index) => {
+		list.splice(index, 1);
+		addTask();
+		getList();
+	};
+	return (
+		<div className="d-flex justify-content-center">
+			<Card className="col-3">
+				<CardBody>
+					<CardTitle className="text-center" tag="h5">
+						To do list with React-Fetch
+					</CardTitle>
+					<ul>
+						{list.map((key, index) => (
+							<li key={index} className="task">
+								{list[index].label}
+								<a
+									className="delete"
+									onClick={() => deleteTask(index, key)}>
+									<FontAwesomeIcon icon={faBackspace} />
+								</a>
+							</li>
+						))}
+					</ul>
+					<div id="inputArea" className="d-flex flex-column">
+						<input
+							id="taskInput"
+							type="text"
+							placeholder="Type your new task here"
+							onChange={(e) => {
+								setnewTask(e.target.value);
+							}}></input>
+						<Button
+							className="btn-sm"
+							color="success"
+							onClick={() => {
+								addTask();
+								console.log(newtask);
+								document.getElementById("taskInput").value = "";
+							}}>
+							Add task
+						</Button>
+					</div>
+				</CardBody>
+			</Card>
+		</div>
+	);
+};
+
+export default ListCard;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import ListCard from "../component/card.jsx";
 
 //create your first component
 const Home = () => {
+	const [list, setList] = useState([]);
+
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="mt-5">
+			<ListCard list={list} setList={setList} />
 		</div>
 	);
 };
